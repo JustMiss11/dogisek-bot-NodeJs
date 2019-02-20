072a8cd42bb0adb7f5fbeb6ceb424b3c826e6fd9
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-02-20 16:25:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ bot.on("ready", async () => {
     // bot.user.setActivity("Hello", {type: "STREAMING", url:"https://twitch.tv/Strandable"});
 
        let statuses = [
-          `to ${bot.guilds.size} servers!`, //shows in how many servers I am!
-          "my prefix is !=", //my Prefix!
-          "start with !=help 1", //simple help!
+       //   `to ${bot.guilds.size} servers!`, //shows in how many servers I am!
+          "můj prefix je >", //my Prefix!
+          "začni s >help", //simple help!
           "Version: 3.3-alpha!", //Bots version!
-          `over ${bot.users.size} users!` //shows how many users im managing!
+          `kolem ${bot.users.size} lidí!` //shows how many users im managing!
        ]
 
     setInterval(function() {
@@ -20,8 +20,29 @@ bot.on("ready", async () => {
         bot.user.setActivity(status, {type: "LISTENING"});
         bot.user.setStatus("idle"); //online, idle, dnd
     }, 5000)
-})
+});
 
 
+bot.on("message" async () => {
+       if(message.author.bot) return;
+       if(!message.content.startsWith(prefix)) return;
+       if(message.channel.dm) return message.author.send("Commands wont work in here.");
+       let messageArray = message.content.split(" ");
+       let msg = messageArray[0];
+       let content = message.content;
+       let author = message.author.username;
+       let user = message.mentions.members.first();
+       
+       if(msg.content.startsWith('Dogisek Bot')) {
+              var embed = new Discord.RichEmbed()
+              .setAuthor("Dogisek Bot!", message.author.avatarURL)
+              .setColor("BLUE")
+              .addField("Prefix:", ">")
+              .setTimestamp()
+              .setFooter("JustNela je best xd");
+              message.channel.send(embed)
+              return;
+       }
+}
        
 bot.login(process.env.TOKEN)
