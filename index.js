28ab26704c37773b289ab71a7a3a4946592d08bb
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-03-02 03:17:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ bot.on("message", message => {
               
                      msg.react("❌");
                      msg.react("✅");
-                     };
+                     });
               return;
        }
        
