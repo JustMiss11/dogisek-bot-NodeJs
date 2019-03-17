1ec8c26d00d556e5063e9ee9252fc725bdea851c
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-03-17 01:50:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -822,7 +822,12 @@ if (message.content.startsWith(prefix + 'queue')) {
  //   id = message.id;
 
 
-bot.on("messageReactionAdd", m => { if(m.emoji.identifier === "🗑") m.message.delete(); })
+bot.on("messageReactionAdd", (messageReaction, user) => {
+       if (messageReaction.emoji.name == "wastebasket") {
+    messageReaction.message.delete();
+  })
+       
+       // => { if(m.emoji.identifier === "🗑") m.message.delete(); })
 	//(reaction.emoji.name === "🗑" && reaction.message.id === msgID){
         //   reaction.message.delete()
           // message.delete()
